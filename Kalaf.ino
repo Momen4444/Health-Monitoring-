1bdbe99d25f6273812c1a7cb3704fa802ce8a65d
Mo'men Qatr <148082590+Momen4444@users.noreply.github.com>
2024-05-08 00:10:52 +0300

    تم هندلة الكود بنجاح🔥

--- Kalaf.ino
+++ Kalaf.ino
@@ -1,102 +1,10 @@
-// #include <Wire.h>
-// #include "MAX30105.h"
-
-// MAX30105 particleSensor;
-
-// const int sampleTime_ms = 100; // Sampling time in milliseconds
-// const int numSamples = 10; // Number of samples to average heart rate
-// uint32_t lastTime = 0;
-// float heartRate = 0;
-// float irValue; // Variable to store raw IR sensor reading
-// float redValue; // Variable to store raw red sensor reading
-// const int buzzerPin = 8; // Pin connected to the buzzer
-// const int heartbeatThreshold = 100; // Threshold for heartbeat detection
-// const int heartbeatmin = 30;
-// const float heartRateMultiplier = 0.23; // Multiplication factor for heart rate calculation
-// void setup() {
-//   Serial.begin(115200);
-//   pinMode(buzzerPin, OUTPUT); // Set the buzzer pin as an output
-
-//   particleSensor.setup();
-//   particleSensor.setPulseAmplitudeRed(0x0A); // Turn Red LED to low to indicate sensor is running
-//   particleSensor.setPulseAmplitudeIR(0x0A); // Turn IR LED to low to indicate sensor is running
-// }
-
-// void loop() {
-//  // Take samples every sampleTime_ms milliseconds
-//   while (!particleSensor.begin(Wire, I2C_SPEED_FAST)) {
-//     Serial.println("MAX30105 not found. Please check wiring/power.");
-    
-//   }
-//   if (millis() - lastTime > sampleTime_ms) {
-//     lastTime = millis();
-    
-//     // Read IR and Red sensor values
-//     irValue = particleSensor.getIR();
-//     redValue = particleSensor.getRed();
-    
-//     // Calculate heart rate from IR sensor values
-//     float avgValue = 0;
-//     for (int i = 0; i < numSamples; i++) {
-//       avgValue += irValue;
-//       delay(2);
-//     }
-//     avgValue /= numSamples;
-//      float heartBeat = (avgValue * 5.0) / 1024.0;
-//     heartRate = 60000 / heartBeat * heartRateMultiplier; // Adjusted multiplication factor
-    
-//     // Output the heart rate
-    
-//     // Check if heartbeat is detected and activate buzzer
-//     if (heartRate > heartbeatThreshold||heartRate<heartbeatmin) {
-
-//       digitalWrite(buzzerPin, HIGH); // Turn on the buzzer
-//     } else {
-//        Serial.println(heartRate);
-//       digitalWrite(buzzerPin, LOW); // Turn off the buzzer
-//     }
-//   }
-  
-// }
-/*
-Optical SP02 Detection (SPK Algorithm) using the MAX30105 Breakout
-By: Nathan Seidle @ SparkFun Electronics
-Date: October 19th, 2016
-https://github.com/sparkfun/MAX30105_Breakout
-
-This demo shows heart rate and SPO2 levels.
-
-It is best to attach the sensor to your finger using a rubber band or other tightening
-device. Humans are generally bad at applying constant pressure to a thing. When you
-press your finger against the sensor it varies enough to cause the blood in your
-finger to flow differently which causes the sensor readings to go wonky.
-
-This example is based on MAXREFDES117 and RD117_LILYPAD.ino from Maxim. Their example
-was modified to work with the SparkFun MAX30105 library and to compile under Arduino 1.6.11
-Please see license file for more info.
-
-Hardware Connections (Breakoutboard to Arduino):
--5V = 5V (3.3V is allowed)
--GND = GND
--SDA = A4 (or SDA)
--SCL = A5 (or SCL)
--INT = Not connected
-
-The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
-but it will also run at 3.3V.
-*/
-
 #include <Wire.h>
 #include "MAX30105.h"
 #include "spo2_algorithm.h"
 
 MAX30105 particleSensor;
 
-#define MAX_BRIGHTNESS 255
-
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
-//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
-//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
 uint16_t irBuffer[100]; //infrared LED sensor data
 uint16_t redBuffer[100]; //red LED sensor data
 #else
@@ -112,100 +20,159 @@ int8_t validHeartRate; //indicator to show if the heart rate calculation is vali
 
 byte pulseLED = 11; //Must be on PWM pin
 byte readLED = 13; //Blinks with each data read
+const int buzzerPin = 4; // Pin connected to the buzzer
+const int trigPin = 10; // Trigger pin of ultrasonic sensor
+const int echoPin = 8; // Echo pin of ultrasonic sensor
+const int sampleTime_ms = 100; // Sampling time in milliseconds
+const int numSamples = 10; // Number of samples to average heart rate
+uint32_t lastTime = 0;
+float irValue; // Variable to store raw IR sensor reading
+float redValue; // Variable to store raw red sensor reading
+const int heartbeatThreshold = 100; // Threshold for heartbeat detection
+const int heartbeatmin = 30;
 
-void setup()
-{
-Serial.begin(115200); // initialize serial communication at 115200 bits per second:
+void setup() {
+  Serial.begin(115200);
+  pinMode(pulseLED, OUTPUT);
+  pinMode(readLED, OUTPUT);
+  pinMode(trigPin, OUTPUT); // Set trigger pin as output
+  pinMode(echoPin, INPUT); // Set echo pin as input
+  pinMode(buzzerPin, OUTPUT); // Set buzzer pin as output
 
-pinMode(pulseLED, OUTPUT);
-pinMode(readLED, OUTPUT);
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) {
+    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
+    while (1);
+  }
 
-// Initialize sensor
-if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
-{
-Serial.println(F("MAX30105 was not found. Please check wiring/power."));
-while (1);
+  byte ledBrightness = 60;
+  byte sampleAverage = 4;
+  byte ledMode = 2;
+  byte sampleRate = 100;
+  int pulseWidth = 411;
+  int adcRange = 4096;
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
+  particleSensor.setPulseAmplitudeRed(0x0A); // Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeIR(0x0A); // Turn IR LED to low to indicate sensor is running
+
+  Serial.println(F("Attach sensor to finger and press any key to start conversion"));
+  while (Serial.available() == 0) ;
+  Serial.read();
 }
 
-Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
-while (Serial.available() == 0) ; //wait until user presses a key
-Serial.read();
+void loop() {
+  
+  String heartRateOutput = printHeartRate(); // Get heart rate output
+  String postureOutput = checkPosture(); // Get posture output
+  SpO2();
+  // Concatenate SpO2, heart rate, and posture information
+  String dataOutput = "SpO2: " + String(spo2) + "\tHeart Rate: " + heartRateOutput + "\tPosture: " + postureOutput;
 
-byte ledBrightness = 60; //Options: 0=Off to 255=50mA
-byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
-byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-int pulseWidth = 411; //Options: 69, 118, 215, 411
-int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+  // Print all data on the same line
+  Serial.println(dataOutput);
 
-particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  delay(10); // Add delay to avoid rapid serial output
 }
 
-void loop()
-{
-bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+void SpO2() {
+  bufferLength = 100;
 
-//read the first 100 samples, and determine the signal range
-for (byte i = 0 ; i < bufferLength ; i++)
-{
-while (particleSensor.available() == false) //do we have new data?
-particleSensor.check(); //Check the sensor for new data
+  for (byte i = 0 ; i < bufferLength ; i++) {
+    while (particleSensor.available() == false)
+      particleSensor.check();
 
-redBuffer[i] = particleSensor.getRed();
-irBuffer[i] = particleSensor.getIR();
-particleSensor.nextSample(); //We're finished with this sample so move to next sample
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample();
 
-Serial.print(F("red="));
-Serial.print(redBuffer[i], DEC);
-Serial.print(F(", ir="));
-Serial.println(irBuffer[i], DEC);
+    Serial.print(F("red="));
+    Serial.print(redBuffer[i], DEC);
+    Serial.print(F(", ir="));
+    Serial.println(irBuffer[i], DEC);
+  }
+
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+  while (1) {
+    for (byte i = 25; i < 100; i++) {
+      redBuffer[i - 25] = redBuffer[i];
+      irBuffer[i - 25] = irBuffer[i];
+    }
+
+    for (byte i = 75; i < 100; i++) {
+      while (particleSensor.available() == false)
+        particleSensor.check();
+
+      digitalWrite(readLED, !digitalRead(readLED));
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample();
+
+      Serial.print(F("red="));
+      Serial.print(redBuffer[i], DEC);
+      Serial.print(F(", ir="));
+      Serial.print(irBuffer[i], DEC);
+
+      Serial.print(F(", HR="));
+      Serial.print(heartRate, DEC);
+
+      Serial.print(F(", HRvalid="));
+      Serial.print(validHeartRate, DEC);
+
+      Serial.print(F(", SPO2="));
+      Serial.print(spo2, DEC);
+
+      Serial.print(F(", SPO2Valid="));
+      Serial.println(validSPO2, DEC);
+    }
+
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+  }
 }
 
-//calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
-maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
-
-//Continuously taking samples from MAX30102. Heart rate and SpO2 are calculated every 1 second
-while (1)
-{
-//dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
-for (byte i = 25; i < 100; i++)
-{
-redBuffer[i - 25] = redBuffer[i];
-irBuffer[i - 25] = irBuffer[i];
+String printHeartRate() {
+  if (millis() - lastTime > sampleTime_ms) {
+    lastTime = millis();
+    irValue = particleSensor.getIR();
+    redValue = particleSensor.getRed();
+    float avgValue = 0;
+    for (int i = 0; i < numSamples; i++) {
+      avgValue += irValue;
+      delay(2);
+    }
+    avgValue /= numSamples;
+    float heartBeat = (avgValue * 5.0) / 1024.0;
+    heartRate = 60000 / heartBeat * 0.18;
+    return String(heartRate); // Return heart rate as a string
+  }
+  return ""; // Return empty string if not ready to print heart rate
 }
 
-//take 25 sets of samples before calculating the heart rate.
-for (byte i = 75; i < 100; i++)
-{
-while (particleSensor.available() == false) //do we have new data?
-particleSensor.check(); //Check the sensor for new data
-
-digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
-
-redBuffer[i] = particleSensor.getRed();
-irBuffer[i] = particleSensor.getIR();
-particleSensor.nextSample(); //We're finished with this sample so move to next sample
-
-//send samples and calculation result to terminal program through UART
-Serial.print(F("red="));
-Serial.print(redBuffer[i], DEC);
-Serial.print(F(", ir="));
-Serial.print(irBuffer[i], DEC);
-
-Serial.print(F(", HR="));
-Serial.print(heartRate, DEC);
-
-Serial.print(F(", HRvalid="));
-Serial.print(validHeartRate, DEC);
-
-Serial.print(F(", SPO2="));
-Serial.print(spo2, DEC);
-
-Serial.print(F(", SPO2Valid="));
-Serial.println(validSPO2, DEC);
-}
-
-//After gathering 25 new samples recalculate HR and SP02
-maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
-}
+String checkPosture() {
+  long duration, distance;
+  String postureStatus;
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  
+  if (distance > 100)
+    return "";  // Return empty string when distance is too large
+  
+  if (distance >= 15) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(100);
+    digitalWrite(buzzerPin, LOW);
+    postureStatus = "False";
+  } else {
+    postureStatus = "True";
+  }
+  
+  return postureStatus;
 }
